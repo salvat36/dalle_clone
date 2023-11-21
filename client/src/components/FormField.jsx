a3225fab4380b68774325424b97c7aad35c1a4ca
ryan salvato <salvat36@gmail.com>
2023-11-21 05:20:09 -0600

    need to correct issue with form field placeholder

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
     <div>
-      <div className='flex items-center gap-2 mb-2'>
+      <div className="flex items-center gap-2 mb-2">
         <label
-        htmlFor={name}
-        className='block text-sm font-medium text-gray-900'>
-          {LabelName}
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
         </label>
-        { isSurpriseMe && (
+        {isSurpriseMe && (
           <button
-          type='button'
-          onClick={handleSurpriseMe}
-          className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
           >
             Surprise me
           </button>
         )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] w-full p-3 outline-none"
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        autoComplete="off"
+      ></input>
     </div>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;
