44a69921975ee420aa5ddfbb420cb2020c1e54c6
ryan salvato <salvat36@gmail.com>
2023-11-21 06:05:02 -0600

    include generate and share with com buttons

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,6 +19,7 @@ const CreatePost = () => {
   const handleSubmit = () => {};
   const handleChange = (e) => {};
   const handleSurpriseMe = () => {};
+  const generateImg = () => {}
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -71,6 +72,22 @@ const CreatePost = () => {
               </div>
             )}
           </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button type='button' onClick={generateImg} className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5" >
+              {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+              Once you have created the image you want, you can share it with others in the community
+          </p>
+          <button
+          type='submit'
+          className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the community'}
+          </button>
         </div>
       </form>
     </section>
