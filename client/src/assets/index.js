24c093355dee58369f29bab3b43bcf0caaf0acda
ryan salvato <salvat36@gmail.com>
2023-11-21 05:54:30 -0600

    add rlogic to render prompt image

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,1 +1,4 @@
+import preview from "./preview.png";
+
+export { preview }
 export { default as logo } from "./logo.svg";
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 // import { useNavigate } from "react-router-dom";
-// import { preview } from "../assets";
+import { preview } from "../assets";
 import { getRandomPrompt } from "../utils";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -49,6 +49,28 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/21 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
         </div>
       </form>
     </section>
